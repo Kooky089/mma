51d480a3794409cc9baff39c9a68a84e3fcd09d6
Volker Jacht <kooky089@gmail.com>
2019-07-17 21:16:48 +0200

    split cmakefiles

--- tests/mma.F90
+++ tests/mma.F90
@@ -2,21 +2,16 @@
 
 program main
    use mma
-   use mpi
    implicit none
    integer :: ierr
    type(MMAComm), pointer :: comm
-   integer :: index
-   integer :: myrank
    integer :: ierror
    call mmacommregister("test", ierror)
    call mmacommprint(ierror)
    call mmacomminitialize(ierror)
    call mmacommget("test", comm, ierror)
-   call MPI_COMM_RANK(comm%comm, myrank, ierror)
    call mmacommprint(ierror)
-      call mmacommregister("test", ierror)
-      write(*,*) ierror
+   call mmacommregister("test", ierror)
    call mmacommfinalize(ierror)
 end program
 
