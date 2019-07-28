0d8f868d78b8f3c20338307ea705596712643e44
Volker Jacht <kooky089@gmail.com>
2019-07-29 00:09:32 +0200

    typo

--- examples/ex2.F90
+++ examples/ex2.F90
@@ -12,7 +12,7 @@ program main
    call mma_comm_register("test", ierror)
    call mma_print(ierror)
    call mma_initialize(ierror)
-   call mm_comm_get("test", comm, ierror)
+   call mma_comm_get("test", comm, ierror)
    call MPI_COMM_RANK(comm%comm, myrank, ierror)
    write(*,*) comm%rank, myrank
    call mma_print(ierror)
